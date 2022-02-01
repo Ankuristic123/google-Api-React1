da7cd0cd14a0c59ee4a630c44d29c3b3527ecdf7
ankur <ankur.sh2011@gmail.com>
2022-02-01 20:10:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import GoogleMap from './components/GoogleMap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Maps App</h1>
+      <GoogleMap />
+      <GoogleMap API/>
+    </div>
+  );
+}
+
+export default App;
